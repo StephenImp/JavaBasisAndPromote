227ea4f0fc2953426e2edb0be5eb20116dcc7427
StephenImp <stephenpengwen@163.com>
2019-01-11 17:30:03 +0800

    提交代码

---
+++ JavaApplication/src/testProblem/TestFloat.java
@@ -0,0 +1,32 @@
+package testProblem;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by MOZi on 2018/12/25.
+ *
+ * 保留两位小数
+ */
+public class TestFloat {
+
+    public static void main(String[] args) {
+
+        Float total = new Float(93.0);
+        Float total2 = new Float(94.0);
+        Float total3 = new Float(96.0);
+
+
+        double t =(total*0.3+total2*0.5+total3*0.2);
+
+        BigDecimal bg = new BigDecimal(t);
+
+        double f1 = bg.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+
+        System.out.println(t);
+
+        System.out.println(f1);
+
+
+
+    }
+}
