321c36207559980525dcd51ef8d27cd9ac22bdbb
imp <lmp@douyu.tv>
2020-05-07 14:11:21 +0800

    update

--- Java-Threadpool-Spring/src/main/java/com/cn/springthreadpool/service/PushMessageService.java
+++ Java-Threadpool-Spring/src/main/java/com/cn/springthreadpool/service/PushMessageService.java
@@ -4,12 +4,12 @@ import com.cn.springthreadpool.entity.Page;
 import com.cn.springthreadpool.entity.PageViewData;
 import com.cn.springthreadpool.entity.PushMessage;
 import com.cn.springthreadpool.entity.QueryDto;
-import org.springframework.stereotype.Service;
+//import org.springframework.stereotype.Service;
 
 /**
  * 模拟业务处理
  */
-@Service
+//@Service
 public interface PushMessageService {
 
     Page<PushMessage> pagedQuery(QueryDto queryDto);
---
+++ readme.txt
@@ -0,0 +1,4 @@
+clone 工程下来后，三个maven工程报错
+解决:
+    1.在项目根目录中把三个maven工程剔除
+    2.在各自的module中添加各自的根目录(add content root)
